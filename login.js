2469c096ebec5a065b42fa277015be591c3a867a
gioelesciacovelli <gioele_s@icloud.com>
2023-01-27 19:27:52 +0100

    final

--- login.js
+++ login.js
@@ -1,15 +1,15 @@
 function checkPassword(event) {
-  fetch("login.json") // Stellt Verbindung zu JSON File her
-      .then(response => response.json()) // Antwort wird empfangen und als JSON weitergegeben
+  fetch("login.json")
+      .then(response => response.json())
       .then(data => {
-        const correctEmail = data.email; // Die Email und Passwort aus dem JSON File werden ausgelesen und Konstanten zugewiesen
+        const correctEmail = data.email;
         const correctPw = data.password;
 
         const email = document.getElementById("email").value; // Das Eingegebene Login wird ausgelesen und als Konstanten definiert
         const password = document.getElementById("password").value;
 
         if (email === correctEmail && password === correctPw) {
-          window.location.href="LoginDone.html"//Wenn Email und Passwort aus dem JSON File mit denen aus dem Login übereinstimmen wird man weitergeleitet
+          window.location.href="logged_in!.html"//Wenn Email und Passwort aus dem JSON File mit denen aus dem Login übereinstimmen wird man weitergeleitet
         }else{
           alert("Falsches Login") //Eine Fehlermeldung erscheint wenn Das Login nicht übereinstimmt
         }
@@ -55,3 +55,28 @@ function validateForm() {
         document.querySelector('#submit').disabled = true;
     }
 }
+
+
+// generate random numbers for the calculation
+var num1 = Math.floor(Math.random() * 10);
+var num2 = Math.floor(Math.random() * 10);
+// store the correct answer to the calculation
+var answer = num1 + num2;
+// display the numbers in the question
+document.getElementById("num1").innerHTML = num1;
+document.getElementById("num2").innerHTML = num2;
+// get the submit button
+var submitButton = document.getElementById("submit-button");
+// add an event listener to the button to check the answer when clicked
+submitButton.addEventListener("click", checkAnswer);
+function checkAnswer() {
+    // get the user's answer from the input field
+    var userAnswer = document.getElementById("captcha-answer").value;
+    // check if the user's answer is correct
+    if (userAnswer == answer) {
+        document.getElementById("result").innerHTML = "Captcha passed!";
+    } else {
+        document.getElementById("result").innerHTML = "Incorrect, please try again.";
+    }
+}
+
