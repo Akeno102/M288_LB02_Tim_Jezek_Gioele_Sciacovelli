11184a51331c8b810b6baf9f47ea4df7a58580e1
gioelesciacovelli <gioele_s@icloud.com>
2023-01-27 15:35:00 +0100

    1

--- login.js
+++ login.js
@@ -1,69 +1,10 @@
-// check password strength
-var password = document.getElementById("password");
-var message = document.getElementById("message");
-password.addEventListener("input", function() {
-  var passwordValue = password.value;
-  if (passwordValue.length < 8) {
-    message.innerHTML = "Weak";
-  } else if (passwordValue.length >= 8 && passwordValue.length < 12) {
-    message.innerHTML = "Could be better";
+function validatePassword() {
+  var password = document.getElementById("password").value;
+  var passwordError = document.getElementById("password-error");
+  if (password.length < 8) {
+    passwordError.innerHTML = "Password is too weak.";
+    passwordError.style.color = "red";
   } else {
-    message.innerHTML = "Strong";
+    passwordError.innerHTML = "";
   }
-});
-
-
-
-
-/*
-//login form submit event
-document.getElementById("login-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    var message = document.getElementById("message");
-  
-    var users = JSON.parse(localStorage.getItem("users")) || [];
-    
-    // check if email and password match with a user
-    var user = users.find(function(user) {
-      return user.email === email && user.password === password;
-    });
-  
-    if (user) {
-      message.innerHTML = "Login successful!";
-    } else {
-      message.innerHTML = "Invalid email or password. Please try again.";
-    }
-  });
-  
-  //signup button click event
-  document.getElementById("signup-btn").addEventListener("click", function() {
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    var message = document.getElementById("message");
-    
-    var users = JSON.parse(localStorage.getItem("users")) || [];
-  
-    var existingUser = users.find(function(user) {
-      return user.email === email;
-    });
-  
-    if (existingUser) {
-      message.innerHTML = "User with this email already exists.";
-    } else {
-      users.push({ email: email, password: password });
-      localStorage.setItem("users", JSON.stringify(users));
-      message.innerHTML = "Signup successful! You can now login.";
-    }
-    fetch('users.json')
-    .then(response => response.json())
-    .then(data => {
-      // do something with the data
-    })
-    .catch(error => {
-      console.log('Error:', error);
-    });
-
-  });
-  */
+}
